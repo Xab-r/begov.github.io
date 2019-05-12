466d770ed0afa275a43645385fe498075fc4413f
begov <50551212+begov@users.noreply.github.com>
2019-05-12 18:48:29 +0300

    Добавил файлы
    Второй сайт

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+var menu = document.getElementById('menu');
+var block = document.getElementById('close');
+var open = document.getElementById('open');
+
+document.getElementById('open').onclick = function(){
+    block.style.transition = '2s';
+    menu.style.transform = 'translateX(0%)';
+    block.style.opacity = '1';
+    open.style.opacity = '0';
+}
+document.getElementById('close').onclick = function(){
+    this.style.transition = '0s';
+    menu.style.transform = 'translateX(-100%) rotate(-180deg)';
+    block.style.opacity = '0';
+    open.style.opacity = '1';
+}
