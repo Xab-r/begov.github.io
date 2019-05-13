896c490f775eb4463f265e95964fa065b2f79eb2
begov <50551212+begov@users.noreply.github.com>
2019-05-13 13:46:03 +0300

    a
    a

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ var openP = document.getElementById('open');
 var closeP = document.getElementById('close');
 
 openP.onclick = function(){
-    menu.style.transform = 'translate(0%)';
+    menu.style.transform = 'translate(0%) rotate(0deg)';
     this.style.opacity = '0';
     closeP.style.opacity = '1';
 }
 closeP.onclick = function(){
-    menu.style.transform = 'translate(-100%)';
+    menu.style.transform = 'translate(-100%)  rotate(-180deg)';
     openP.style.opacity = '1';
     closeP.style.opacity = '0';
 }
